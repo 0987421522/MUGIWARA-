35101aa8b825cc041060e346f647d43e64ab84fb
MugiwaraLuffy <116393673+0987421522@users.noreply.github.com>
2026-06-23 11:47:58 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,7 @@ android {
 
 dependencies {
     // AndroidX Core
+    implementation("androidx.startup:startup-runtime:1.1.3")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
